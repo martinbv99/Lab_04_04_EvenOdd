c83f3e56fd727053fc8390b357697ce6cfadabc3
martinbv99 <martinbv@mail.uc.edu>
2024-09-19 17:40:50 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main
+
+{
+    public static void main(String[] args)
+    {
+        int numToExamine = 2;
+        int MODULO = 2;
+        int sum = 0;
+
+        sum = numToExamine % MODULO;
+        System.out.println("The modulo of two integers " + MODULO + " and " + numToExamine + " is " + sum );
+        System.out.println("Since the modulo is 0 that means 2 is even.");
+
+
+
+
+
+
+    }
+}
